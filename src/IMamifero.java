3d2adf59057198a5df624b36a27149b3419954b7
noesnestor <88690061+noesnestor@users.noreply.github.com>
2022-03-27 21:30:16 -0300

    agregada interfaz mamifero

---
+++ src/IMamifero.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Néstor
+ */
+public interface IMamifero 
+{
+    public String getNombre();
+    public boolean respondeA(String unNombre);
+    
+    public void caminar();
+    public void correr();
+    public void saltar();
+}
