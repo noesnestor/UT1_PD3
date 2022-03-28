81f496c1f1edd12e7657508b50490d863cb4c375
noesnestor <88690061+noesnestor@users.noreply.github.com>
2022-03-27 21:13:10 -0300

    Agregadas funcionalidades clase Perro

--- src/Perro.java
+++ src/Perro.java
@@ -8,5 +8,23 @@
  * @author Néstor
  */
 public class Perro {
+    private String nombre;
+    
+    public Perro (String nombre)
+    {
+        this.nombre = nombre;
+    }
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+    public void ladrar()
+    {
+        System.out.println("wof");
+    }
+    public boolean respondeA(String unNombre)
+    {
+        return this.nombre.equals(unNombre)
+    }
     
 }
