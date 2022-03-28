3aaaea1cedc9db6e55aeb70373ab876ca7e891aa
Néstor Martínez <nestor.marvi@gmail.com>
2022-03-27 21:06:13 -0300

    MainMaquinaB

--- src/main.java
+++ src/main.java
@@ -12,8 +12,9 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        
     }
     
 }
