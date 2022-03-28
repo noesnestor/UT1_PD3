28901619fac653d25fa8891e283361b40372f220
Néstor Martínez <nestor.marvi@gmail.com>
2022-03-27 21:00:33 -0300

    MainMaquinaA

---
+++ src/Perro.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Néstor
+ */
+public class Perro {
+    
+}
